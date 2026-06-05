4f05c7cddf48e3d27622501a6d2d1a194ca8d09a
Dav-ax <dravir.ax@gmail.com>
2026-06-05 15:01:19 -0500

    Add clock page

---
+++ src/pages/ClockPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DigitalClockTimezones from '../components/DigitalClockTimezones';
+
+const ClockPage: React.FC = () => {
+  return (
+    <div>
+      <DigitalClockTimezones />
+    </div>
+  );
+};
+
+export default ClockPage;
